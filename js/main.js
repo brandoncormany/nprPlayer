994996e3f372b83046b944bae3f3ed4349fb66da
Brandon Cormany <brandoncormany@gmail.com>
2014-08-17 21:48:16 -0700

    After completing part 2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,31 @@
 var app = angular.module('myApp', []);
+
+app.run(function($rootScope){
+  $rootScope.name = "Brandon ";
+});
+app.controller('MyController', function($scope){
+  $scope.person = {
+    name: "Ari Lerner"
+  }
+});
+app.controller('PlayerController', ['$scope', function($scope) {
+  $scope.playing = false;
+  $scope.audio = document.createElement('audio');
+  $scope.audio.src = '/media/npr.mp4';
+  $scope.play = function() {
+    $scope.audio.play();
+    $scope.playing = true;
+  };
+  $scope.stop = function() {
+    $scope.audio.pause();
+    $scope.playing = false;
+  };
+  $scope.audio.addEventListener('ended', function() {
+    $scope.$apply(function() {
+      $scope.stop()
+    });
+  });
+}]);
+app.controller('RelatedController', ['$scope', function($scope) {
+
+}]);
