ac61a3103aae1371734db93a32e3abd0536fb8a6
Brandon Cormany <brandoncormany@gmail.com>
2014-08-19 12:20:12 -0700

    part 3

--- js/main.js
+++ js/main.js
@@ -8,7 +8,11 @@ app.controller('MyController', function($scope){
     name: "Ari Lerner"
   }
 });
-app.controller('PlayerController', ['$scope', function($scope) {
+
+var apiKey = 'MDE2MjMzOTUxMDE0MDgzOTk1NzMxZDZiOQ001',
+  nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
+
+app.controller('PlayerController', function($scope, $http) {
   $scope.playing = false;
   $scope.audio = document.createElement('audio');
   $scope.audio.src = '/media/npr.mp4';
@@ -25,7 +29,25 @@ app.controller('PlayerController', ['$scope', function($scope) {
       $scope.stop()
     });
   });
-}]);
+  // construct our http request
+  $http({
+    method: 'JSON',
+    url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
+  }).success(function(data, status) {
+    // Now we have a list of the stories (data.list.story)
+    // in the data object that the NPR API
+    // returns in JSON that looks like:
+    // data: { "list": {
+    //   "title": ...
+    //   "story": [
+    //     { "id": ...
+    //       "title": ...
+    $scope.programs = data.list.story;
+  }).error(function(data, status) {
+    // Some error occurred
+  });
+});
+
 app.controller('RelatedController', ['$scope', function($scope) {
 
 }]);
